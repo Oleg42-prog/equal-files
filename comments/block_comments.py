4501773f3b5ce0e028e81038a7569d25cea56600
Oleg Dudnik <oleggelo86@gmail.com>
2024-06-18 19:12:04 +0300

    ✨ (block_comments.py): Add remove_block_comments implementation modified:   comments/block_comments.py

--- comments/block_comments.py
+++ comments/block_comments.py
@@ -3,4 +3,18 @@ def remove_block_comments(
     block_comment_start_char: str,
     block_comment_end_char: str
 ):
-    raise NotImplementedError
+    depth = 0
+    new_text = []
+    for char in text:
+
+        if char == block_comment_start_char:
+            depth += 1
+
+        if char == block_comment_end_char:
+            if depth > 0:
+                depth -= 1
+
+        if depth == 0:
+            new_text.append(char)
+
+    return ''.join(new_text)
