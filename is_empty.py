e0f7614948ba8db386b673accf998a864fe49df3
Oleg Dudnik <oleggelo86@gmail.com>
2024-06-18 18:11:12 +0300

    🚧 (is_empty.py): Define the signature of the is_empty function new file:   is_empty.py

---
+++ is_empty.py
@@ -0,0 +1,10 @@
+def is_empty(
+    file_path: str,
+    ignore_whitespaces=False,
+    ignore_comments=False,
+    line_comment_char='//',
+    block_comment_start_char='/*',
+    block_comment_end_char='*/',
+    encoding='utf-8'
+):
+    raise NotImplementedError
