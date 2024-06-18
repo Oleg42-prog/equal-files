8896b1c15b472be97b0a4ba392d89bb5bd91b157
Oleg Dudnik <oleggelo86@gmail.com>
2024-06-18 18:21:07 +0300

    🚧 (is_empty.py): Split is_empty function into is_empty_file and is_empty_text functions modified:   is_empty.py

--- is_empty.py
+++ is_empty.py
@@ -1,10 +1,30 @@
-def is_empty(
+def is_empty_file(
     file_path: str,
     ignore_whitespaces=False,
     ignore_comments=False,
     line_comment_char='//',
     block_comment_start_char='/*',
     block_comment_end_char='*/',
-    encoding='utf-8'
+    encoding='utf-8',
+    **kwargs
+):
+    with open(file_path, 'r', encoding=encoding, **kwargs) as file:
+        return is_empty_text(
+            text=file.read(),
+            ignore_whitespaces=ignore_whitespaces,
+            ignore_comments=ignore_comments,
+            line_comment_char=line_comment_char,
+            block_comment_start_char=block_comment_start_char,
+            block_comment_end_char=block_comment_end_char
+        )
+
+
+def is_empty_text(
+    text: str,
+    ignore_whitespaces=False,
+    ignore_comments=False,
+    line_comment_char='//',
+    block_comment_start_char='/*',
+    block_comment_end_char='*/'
 ):
     raise NotImplementedError
