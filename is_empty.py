71cd9edd0294fa4d248cff0f179bcdc211677dbc
Oleg Dudnik <oleggelo86@gmail.com>
2024-06-18 18:40:10 +0300

    ♻️ (is_empty.py): Abstract remove_line_comment function modified:   is_empty.py

--- is_empty.py
+++ is_empty.py
@@ -49,10 +49,17 @@ def remove_line_comments(
     new_text = []
 
     for line in text.split('\n'):
-        new_line = first(line.split(line_comment_char))
+        new_line = remove_line_comment(line, line_comment_char)
         new_text.append(new_line)
 
     return '\n'.join(new_text)
+
+
+def remove_line_comment(
+    line: str,
+    line_comment_char: str
+):
+    return first(line.split(line_comment_char))
 
 
 def first(iterable):
