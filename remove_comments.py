6c9d3fdb34d71e4be6c7fa17300e7e7403bdbeb9
Oleg Dudnik <oleggelo86@gmail.com>
2024-06-21 14:20:41 +0300

    🚸 (remove_comments.py): Add CLI for remove comments from files modified:   remove_comments.py

--- remove_comments.py
+++ remove_comments.py
@@ -1,3 +1,5 @@
+import os
+import argparse
 from comments import remove_line_comments
 from comments import remove_block_comments
 from utils import read_file, write_file
@@ -30,3 +32,42 @@ def remove_comments_from_file(
         text = '\n'.join([line for line in text.split('\n') if not line == ''])
 
     write_file(file_path, text, encoding=encoding, **kwargs)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_folder', type=str, required=True)
+    parser.add_argument('--extensions', nargs='+', default=['.c', '.cpp'], type=str)
+    parser.add_argument('--remove_line_comments', action='store_true')
+    parser.add_argument('--remove_block_comments', action='store_true')
+    parser.add_argument('--remove_empty_lines', action='store_true')
+    parser.add_argument('--remove_whitespace_lines', action='store_true')
+    parser.add_argument('--line_comment_char', type=str, default='//')
+    parser.add_argument('--block_comment_start_char', type=str, default='/*')
+    parser.add_argument('--block_comment_end_char', type=str, default='*/')
+    parser.add_argument('--encoding', type=str, default='utf-8')
+    parser.add_argument('--errors', type=str, default='strict')
+    args = parser.parse_args()
+
+    extensions = tuple(args.extensions)
+
+    for root, dirs, files in os.walk(args.input_folder):
+
+        for file_name in files:
+
+            if not file_name.endswith(extensions):
+                continue
+
+            remove_comments_from_file(
+                file_path=os.path.join(root, file_name),
+                line_comments=args.remove_line_comments,
+                block_comments=args.remove_block_comments,
+                remove_empty_lines=args.remove_empty_lines,
+                remove_whitespace_lines=args.remove_whitespace_lines,
+                line_comment_char=args.line_comment_char,
+                block_comment_start_char=args.block_comment_start_char,
+                block_comment_end_char=args.block_comment_end_char,
+                encoding=args.encoding,
+                errors=args.errors
+            )
