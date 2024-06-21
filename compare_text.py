c2709d6a19bd346cce39ad29de306cc52738f6f2
Oleg Dudnik <oleggelo86@gmail.com>
2024-06-21 14:48:28 +0300

    ✨ (compare_text.py): Add compare_text implementation modified:   compare_text.py

--- compare_text.py
+++ compare_text.py
@@ -1,4 +1,6 @@
 from options import CodeComparsionOptions
+from comments import remove_line_comments
+from comments import remove_block_comments
 from utils import read_file
 
 
@@ -19,4 +21,16 @@ def compare_text(
     text_2: str,
     options: CodeComparsionOptions
 ):
-    raise NotImplementedError
+    if options.ignore_line_comments:
+        text_1 = remove_line_comments(text_1, options.line_comment_char)
+        text_2 = remove_line_comments(text_2, options.line_comment_char)
+
+    if options.ignore_block_comments:
+        text_1 = remove_block_comments(text_1, options.block_comment_start_char, options.block_comment_end_char)
+        text_2 = remove_block_comments(text_2, options.block_comment_start_char, options.block_comment_end_char)
+
+    if options.ignore_whitespaces:
+        text_1 = ''.join([char for char in text_1 if not char.isspace()])
+        text_2 = ''.join([char for char in text_2 if not char.isspace()])
+
+    return text_1 == text_2
