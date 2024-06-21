2685c2e0a377389dd4b76555fd4cf00575199248
Oleg Dudnik <oleggelo86@gmail.com>
2024-06-21 12:59:57 +0300

    🚧 (utils.py): Add write_file helper function modified:   utils.py

--- utils.py
+++ utils.py
@@ -5,3 +5,8 @@ def first(iterable):
 def read_file(file_path: str, encoding='utf-8', **kwargs):
     with open(file_path, 'r', encoding=encoding, **kwargs) as file:
         return file.read()
+
+
+def write_file(file_path: str, text: str, encoding='utf-8', **kwargs):
+    with open(file_path, 'w', encoding=encoding, **kwargs) as file:
+        file.write(text)
