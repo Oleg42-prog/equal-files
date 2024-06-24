676ff2bb47868b7afca0b246601a4c3d1333389e
Oleg Dudnik <oleggelo86@gmail.com>
2024-06-24 11:05:06 +0300

    🚸 (compare_text.py): Add argument parser modified:   compare_text.py

--- compare_text.py
+++ compare_text.py
@@ -1,6 +1,6 @@
+import argparse
 from options import CodeComparsionOptions
-from comments import remove_line_comments
-from comments import remove_block_comments
+from comments import remove_line_comments, remove_block_comments
 from utils import read_file
 
 
@@ -34,3 +34,36 @@ def compare_text(
         text_2 = ''.join([char for char in text_2 if not char.isspace()])
 
     return text_1 == text_2
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--basis_folder', type=str, required=True)
+    parser.add_argument('--relative_folder', type=str, required=True)
+    parser.add_argument('--compare_by_name', action='store_true')
+    parser.add_argument('--extensions', nargs='+', default=['.c', '.cpp'], type=str)
+    parser.add_argument('--ignore_whitespaces', action='store_true')
+    parser.add_argument('--ignore_line_comments', action='store_true')
+    parser.add_argument('--ignore_block_comments', action='store_true')
+    parser.add_argument('--line_comment_char', type=str, default='//')
+    parser.add_argument('--block_comment_start_char', type=str, default='/*')
+    parser.add_argument('--block_comment_end_char', type=str, default='*/')
+    parser.add_argument('--encoding', type=str, default='utf-8')
+    parser.add_argument('--errors', type=str, default='strict')
+    parser.add_argument('--show_diff', action='store_true')
+    args = parser.parse_args()
+
+    if args.errors not in ['replace', 'ignore', 'strict']:
+        raise ValueError('The --errors parameter can only be replace, ignore or strict')
+
+    problem_files: list[str] = []
+    extensions = tuple(args.extensions)
+    code_options = CodeComparsionOptions(
+        ignore_whitespaces=args.ignore_whitespaces,
+        ignore_line_comments=args.ignore_line_comments,
+        line_comment_char=args.line_comment_char,
+        ignore_block_comments=args.ignore_block_comments,
+        block_comment_start_char=args.block_comment_start_char,
+        block_comment_end_char=args.block_comment_end_char
+    )
